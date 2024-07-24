8f88d11d146b02780c12a7413e5ebf023307a5df
rida2025 <ridaeljirari@gmail.com>
2024-07-24 22:28:03 +0100

    fix quit

--- Commands/Quit.cpp
+++ Commands/Quit.cpp
@@ -6,7 +6,7 @@
 /*   By: mel-jira <mel-jira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 15:29:59 by mel-jira          #+#    #+#             */
-/*   Updated: 2024/07/23 19:12:56 by mel-jira         ###   ########.fr       */
+/*   Updated: 2024/07/24 22:24:07 by mel-jira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,8 @@ int QUIT_COMMAND(Client &client, std::vector<Channels> &channels){
     std::string channel_to_leave;
     int flag = -1;
     for (size_t i = 0;i < client.inside_channel.size();i++){
+        i = 0;
         flag = doesChannelExist(channels, client.inside_channel[i]);
-        if (flag == -1)
-            continue ;
-        std::cout << "channel name " << channels[flag].channel_name << std::endl;
-        std::cout << "nick name " << client.nickName << std::endl;
         remove_member(channels[flag], client.nickName);
         channels[flag].members_count--;
         if (is_admin(channels[flag], client.inside_channel[i])){
@@ -31,10 +28,10 @@ int QUIT_COMMAND(Client &client, std::vector<Channels> &channels){
                     break ;
                 }
             }
-        } // after this if he was admin he is not anymore
+        }
         client.inside_channel.erase(client.inside_channel.begin()+i);
         buffer = ":" + client.nickName + "!" + client.userName + "@" + client.ip + " QUIT :EOF from client\r\n";
-        broad_cast(channels[flag], buffer, "", ""); // we send to all members the correct respond
+        broad_cast(channels[flag], buffer, "", "");
     }
     return 0;
 }
