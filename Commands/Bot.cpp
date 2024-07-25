03d74522db4c48713f67bad7228eae9b668e2c3f
TakrayoutMohamed <takrayoutm@gmail.com>
2024-07-25 20:34:16 +0100

    update Commands

--- Commands/Bot.cpp
+++ Commands/Bot.cpp
@@ -3,24 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   Bot.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mel-jira <mel-jira@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mohtakra <mohtakra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 07:34:00 by mel-jira          #+#    #+#             */
-/*   Updated: 2024/07/17 07:34:13 by mel-jira         ###   ########.fr       */
+/*   Updated: 2024/07/25 20:30:42 by mohtakra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Commands.hpp"
-
 std::string getRandomLine(const std::vector<std::string>& lines) {
     int index = rand() % lines.size();
     return lines[index];
 }
-
 void    HAKIM_COMMAND(int fd){
     std::string buffer;
-    std::ifstream inputFile("info.txt");
-    if (!inputFile){
+    std::ifstream inputFile;
+    inputFile.open("Commands/info.txt");
+    if (!inputFile.is_open()){
         std::cerr << "Error opening file." << std::endl;
         return ;
     }
@@ -31,9 +30,14 @@ void    HAKIM_COMMAND(int fd){
         lines.push_back(line);
     }
     inputFile.close();
+    if (lines.empty()) {
+        std::cerr << "The file is empty." << std::endl;
+        return;
+    }
     srand(static_cast<unsigned int>(time(0)));
     std::string randomLine = getRandomLine(lines);
-    buffer = ":Bot el-hakim say: " + randomLine + "\r\n";
+    buffer = ":Bot el-hakim say: " + randomLine + "";
+    buffer = ":el-hakim!~elHakim@somewhere PRIVMSG - :"+ buffer + "\r\n";
     send(fd, buffer.c_str(), buffer.length(), 0);
-    std::cout << "Random Line: " << randomLine << std::endl;
+    // std::cout << "Random Line: " << randomLine << std::endl;
 }
